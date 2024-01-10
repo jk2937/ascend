5f0f21ed516cef09a3eab048208b65dab1691a96
jk2937 <kaschakjonathan+github@gmail.com>
2024-01-10 11:04:12 -0800

    Remove old files

--- stefanbeller-tiled_examples-5cc6d49/docs/Automapping/build.sh
+++
@@ -1,9 +0,0 @@
-latex Automapping_rulefiles.tex
-latex Automapping_rulefiles.tex
-latex Automapping_rulefiles.tex
-dvipdfm Automapping_rulefiles.dvi
-
-rm Automapping_rulefiles.aux
-rm Automapping_rulefiles.log
-rm Automapping_rulefiles.dvi
-
